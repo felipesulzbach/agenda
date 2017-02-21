bb42169fd69420a50c4e0a8001653a2b32e1a2cc
Felipe <feliperpdr@yahoo.com.br>
2017-02-21 13:18:24 -0300

    Criado diretorio receiver.

---
+++ app/src/main/java/br/com/felipe/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,13 @@
+package br.com.felipe.agenda.bean;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.Serializable;
+
+/**
+ * Created by felipe on 21/02/2017.
+ */
+public class SMSReceiver extends BroadcastReceiver {
+
+}
